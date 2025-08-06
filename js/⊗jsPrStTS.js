915405e3ccbb2ee14388efbcc0670de5a65d5b32
Vincent-tt-cyber <evgeniy161102@gmail.com>
2025-08-06 18:02:07 +0300

    ffirst commit

---
+++ js/⊗jsPrStTS.js
@@ -0,0 +1,31 @@
+// БД тегов
+const tagsDB = {
+  a: {
+    description:
+      "Является одним из важных элементов HTML и предназначен для создания ссылок",
+    example: '<a href="https://example.com">Пример ссылки</a>',
+  },
+  div: {
+    description:
+      "Элемент <div> является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого.",
+    example: '<a href="https://example.com">Пример ссылки</a>',
+  },
+};
+
+// DOM елементы
+const searchInput = document.querySelector(".input-search");
+const tagName = document.querySelector(".tag-name");
+
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    let value = e.target.value.trim().toLowerCase();
+    showTagInfo(value);
+  }
+});
+
+// Вывод информации о теге
+function showTagInfo(tagName) {
+  if (tagsDB[tagName]) {
+    tagName.textContent = `<${tagName}>`;
+  }
+}
